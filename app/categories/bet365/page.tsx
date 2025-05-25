0a078d3ea17d7606d0bbac4fb716869ce70113ed
Develigent <josephreignhard05@gmail.com>
2025-05-25 16:20:26 +0800

    1

--- app/categories/bet365/page.tsx
+++ app/categories/bet365/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { JSX, useState } from "react";
 import Image from "next/image";
 import React from "react";
 import Bet365Board from "@/components/reusable/bet365board/page";
@@ -28,7 +28,7 @@ const soccerMatchDetails = {
 };
 const basketballMatchDetails = {
   videoSrc: "/video.mp4",
-  sportIconSrc: "/sports_nav/icon_sports04.png", 
+  sportIconSrc: "/sports_nav/icon_sports04.png",
   league: "NBA",
   date: "April 8, 2025 - 18:00",
   odds: [
@@ -44,8 +44,8 @@ const basketballMatchDetails = {
 
 const RacelMatchDetails = {
   videoSrc: "/video.mp4",
-  sportIconSrc: "/sports_nav/icon_sports04.png", 
-  raceIconSrc: "/sports_nav/icon_sports04.png", 
+  sportIconSrc: "/sports_nav/icon_sports04.png",
+  raceIconSrc: "/sports_nav/icon_sports04.png",
   league: "Festival Downs",
   date: "Tue Apr 08 14:48",
   odds: [
